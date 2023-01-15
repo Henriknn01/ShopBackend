1a3aeeb97398e843e78735446fce51d16d7fd059
Henrik <norheimhenrik@gmail.com>
2023-01-15 19:48:21 +0100

    Updated serializers to follow standard

--- ShopCMS/serializers.py
+++ ShopCMS/serializers.py
@@ -14,33 +14,40 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
 class DiscountSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Discount
+        fields = '__all__'
 
 
 class ProductCategorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProductCategory
+        fields = '__all__'
 
 
 class ProductInventorySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = ProductInventory
+        fields = '__all__'
 
 
 class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
 
 
 class OrderDetailsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = OrderDetails
+        fields = '__all__'
 
 
 class OrderItemsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = OrderItems
+        fields = '__all__'
 
 
 class PaymentDetailsSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = PaymentDetails
+        fields = '__all__'
