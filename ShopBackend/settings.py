dfb8f6b858c8ea6f80d355a054d0e67b645946b3
Henrik <norheimhenrik@gmail.com>
2023-05-29 18:29:00 +0200

    added csrf setting

--- ShopBackend/settings.py
+++ ShopBackend/settings.py
@@ -32,6 +32,8 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(" ")
 
+CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS").split(" ")
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -103,7 +105,6 @@ if PRODUCTION == "True":
             },
         }
     }
-
 else:
     DATABASES = {
         "default": {
