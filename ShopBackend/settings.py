6a46a4f84979144e408ef02663977b6090a6872a
0xBACE <0xbace@gmail.com>
2023-05-29 18:13:44 +0200

    added static to url patterns

--- ShopBackend/settings.py
+++ ShopBackend/settings.py
@@ -149,6 +149,7 @@ USE_TZ = True
 
 STATIC_URL = "staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
