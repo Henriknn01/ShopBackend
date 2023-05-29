623dfd04fede08cbc442e8a190c4b652718e6d04
0xBACE <0xbace@gmail.com>
2023-05-29 17:57:49 +0200

    changed static to staticfiles

--- ShopBackend/settings.py
+++ ShopBackend/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
